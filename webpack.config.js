a4905d011e03afb573b699eb1e51bb31f1e9e75b
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2025-02-12 15:23:49 +0530

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ const base = {
             'path': require.resolve('path-browserify'),
             'crypto': require.resolve('crypto-browserify'),
             'stream': require.resolve('stream-browserify'),
-            'buffer': require.resolve('buffer/'),
+            'buffer': require.resolve('buffer'),
             'util': require.resolve('util/'),
             'assert': require.resolve('assert/')
         },
